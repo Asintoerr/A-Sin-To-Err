8bb04f59836e79fa5830c301bd1d4448290ffe7c
Dmitriy Mayorov <arrabon@dimview.org>
2015-10-27 18:31:43 -0400

    Display example

---
+++ display.asm
@@ -0,0 +1,33 @@
+    lit #0
+    out #1
+
+	lit #1 ; digit value
+	out #0
+	lit #8 ; which digit
+	out #1
+    lit #0
+    out #1
+
+	lit #2 ; digit value
+	out #0
+	lit #4 ; which digit
+	out #1
+    lit #0
+    out #1
+
+	lit #3 ; digit value
+	out #0
+	lit #2 ; which digit
+	out #1
+    lit #0
+    out #1
+
+	lit #4 ; digit value
+	out #0
+	lit #1 ; which digit
+	out #1
+    lit #0
+    out #1
+
+halt:
+    jmp halt
