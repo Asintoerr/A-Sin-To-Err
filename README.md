8b9c0c08952d51393496f28289475f3f1a8b3be0
Dmitriy Mayorov <arrabon@dimview.org>
2015-10-29 21:06:26 -0400

    To reduce ROM footprint, C++ code generates assembly language for RC5 rather than #defining expanded key table

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Cipher Machine Using [4 bit Nibbler CPU](http://bigmessofwires.com/nibbler).
 Design highlights
 -----------------
 
-* Seven-segment display, 12 key telephone style keypad
+* Four digit seven-segment LED display
+* 12 key telephone style keypad
 * [RC5](https://en.wikipedia.org/wiki/RC5) in [CTR mode](https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Counter_.28CTR.29)
 
 [Straddling Checkerboard](https://en.wikipedia.org/wiki/VIC_cipher#Straddling_checkerboard)
@@ -18,3 +19,18 @@ A table to convert digits to letters
 |   | A |   | S | I | N |   | T | O | E | R |
 | 1 | B | C | D | F | G | H | J | K | L | M |
 | 5 | P | Q | U | V | W | X | Y | Z | . | / |
+
+Building
+--------
+
+Command-line tools, code generator:
+
+    make straddling_checkerboard.cpp 
+    make a_sin_to_err
+    cp asintoer.asm asintoer2.asm
+    ./a_sin_to_err codegen >>asintoer2.asm 
+
+Compiling asintoer2.asm on Windows to create ROM for Nibbler:
+
+    assembler -o asintoer2.bin asintoerr2.asm
+    
--- a_sin_to_err.cpp
+++ a_sin_to_err.cpp
@@ -4,6 +4,9 @@
 // Output:
 //   8 digit nonce followed by cyphertext or plaintext digits
 //
+// When called with any argument, instead of performing encryption generates Nibbler 
+// assembly code for expanded key.
+//
 #include <stdio.h>
 #include <inttypes.h>
 #include <cstdint>
@@ -55,22 +58,20 @@ void rc5_setup(unsigned char *key_bytes)
 void rc5_encrypt(uint32_t *pin, uint32_t *pout)
 {
   uint32_t A = pin[0] + expanded_key_table[0];
-fprintf(stderr, "A0=0x%08x+0x%08x=0x%08x\n",pin[0],expanded_key_table[0],A);
+//fprintf(stderr, "A0=0x%08x+0x%08x=0x%08x\n",pin[0],expanded_key_table[0],A);
   uint32_t B = pin[1] + expanded_key_table[1];
-fprintf(stderr, "B0=0x%08x+0x%08x=0x%08x\n",pin[1],expanded_key_table[1],B);
+//fprintf(stderr, "B0=0x%08x+0x%08x=0x%08x\n",pin[1],expanded_key_table[1],B);
   for (uint32_t i = 1; i <= r; i++) {
-fprintf(stderr, "A%d = rotl(0x%08x ^ 0x%08x = 0x%08x, 0x%08x) + 0x%08x = 0x%08x + 0x%08x = 0x%08x\n", 
-                   i,         A,       B,     A ^ B,  B, expanded_key_table[2 * i], rotl(A ^ B, B), expanded_key_table[2 * i], rotl(A ^ B, B) + expanded_key_table[2 * i]);
+//fprintf(stderr, "A%d = rotl(0x%08x ^ 0x%08x = 0x%08x, 0x%08x) + 0x%08x = 0x%08x + 0x%08x = 0x%08x\n", 
+//                   i,         A,       B,     A ^ B,  B, expanded_key_table[2 * i], rotl(A ^ B, B), expanded_key_table[2 * i], rotl(A ^ B, B) + expanded_key_table[2 * i]);
     A = rotl(A ^ B, B) + expanded_key_table[2 * i];
-fprintf(stderr, "B%d = rotl(0x%08x ^ 0x%08x= 0x%08x, 0x%08x) + 0x%08x = 0x%08x + 0x%08x = 0x%08x\n", 
-                   i,         B,       A,    B ^ A,   A, expanded_key_table[2 * i + 1], rotl(B ^ A, A), expanded_key_table[2 * i + 1], rotl(B ^ A, A) + expanded_key_table[2 * i + 1]);
+//fprintf(stderr, "B%d = rotl(0x%08x ^ 0x%08x= 0x%08x, 0x%08x) + 0x%08x = 0x%08x + 0x%08x = 0x%08x\n", 
+//                   i,         B,       A,    B ^ A,   A, expanded_key_table[2 * i + 1], rotl(B ^ A, A), expanded_key_table[2 * i + 1], rotl(B ^ A, A) + expanded_key_table[2 * i + 1]);
     B = rotl(B ^ A, A) + expanded_key_table[2 * i + 1];
   }
   pout[0] = A;
   pout[1] = B;
-
-fprintf(stderr, "%08x %08x -> %08x %08x\n",pin[0],pin[1],pout[0],pout[1]);
-
+//fprintf(stderr, "%08x %08x -> %08x %08x\n",pin[0],pin[1],pout[0],pout[1]);
 }
 
 void emit(char c)
@@ -93,35 +94,103 @@ void emit(char c)
   }
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
   rc5_setup(&key[0]);
   
-// Nibbler assembly code generation 
-#if 0
-printf("#define EXPANDED_KEY_TABLE $c0 ; %d 32 bit words = %d nibbles\n", t, t * 8);
-// Order by nibble value to reduce number of lit commands
-std::set<int> nibble_addr[16];
-for (int i = 0; i < t; i++) {
-  nibble_addr[(expanded_key_table[i]      ) & 0xf].insert(i * 8 + 0);
-  nibble_addr[(expanded_key_table[i] >>  4) & 0xf].insert(i * 8 + 1);
-  nibble_addr[(expanded_key_table[i] >>  8) & 0xf].insert(i * 8 + 2);
-  nibble_addr[(expanded_key_table[i] >> 12) & 0xf].insert(i * 8 + 3);
-  nibble_addr[(expanded_key_table[i] >> 16) & 0xf].insert(i * 8 + 4);
-  nibble_addr[(expanded_key_table[i] >> 20) & 0xf].insert(i * 8 + 5);
-  nibble_addr[(expanded_key_table[i] >> 24) & 0xf].insert(i * 8 + 6);
-  nibble_addr[(expanded_key_table[i] >> 28) & 0xf].insert(i * 8 + 7);
-}
-for (int i = 0; i < 16; i++) {
-  printf("    lit #$%x\n", i);
-  for ( std::set<int>::const_iterator it = nibble_addr[i].begin()
-      ; it != nibble_addr[i].end()
-      ; ++it
-      ) {
-    printf("    st EXPANDED_KEY_TABLE+%d\n", *it);
+  if (argc > 1) {
+    // Nibbler assembly code generation 
+    printf("; Automatically generated code follows, see a_sin_to_err.cpp =============================\n"
+           "\n"    
+           "    ld TODO_LIST_A+0\n"
+           "    jnz +\n"
+           "\n");
+    for (int i = 0; i < 8; i++) {
+      printf("    ld CTR_NONCE+%d\n", i);
+      printf("    st OPERAND_A+%d\n", i);
+    }
+    printf("\n");
+    for (int i = 0; i < 8; i++) {
+      printf("    lit #$%x\n", (expanded_key_table[0] >> (i * 4)) & 0xf);
+      printf("    st OPERAND_B+%d\n", i);
+    }
+    printf("\n"
+           "    lit #1\n"
+           "    st TODO_LIST_A+0\n"
+           "    jmp add32_into_rc5_a\n"
+           "\n");
+    printf("+   ld TODO_LIST_B+0\n"
+           "    jnz +\n"
+           "\n");
+    for (int i = 0; i < 8; i++) {
+      printf("    ld CTR_COUNTER+%d\n", i);
+      printf("    st OPERAND_A+%d\n", i);
+    }
+    printf("\n");
+    for (int i = 0; i < 8; i++) {
+      printf("    lit #$%x\n", (expanded_key_table[1] >> (i * 4)) & 0xf);
+      printf("    st OPERAND_B+%d\n", i);
+    }
+    printf("\n"
+           "    lit #1\n"
+           "    st TODO_LIST_B+0\n"
+           "    jmp add32_into_rc5_b\n"
+           "\n");
+    for (int j = 1; j <= 16; j++) {
+      printf("; Round %d\n"
+             "\n", j);
+      printf("+   ld TODO_LIST_AA+%d\n"
+             "    jnz +\n"
+             "\n"
+             "    ld RC5_B+0\n"
+             "    st ROTL_COUNT+0\n"
+             "    ld RC5_B+1\n"
+             "    st ROTL_COUNT+1"
+             "\n"
+             "    lit #1\n"
+             "    st TODO_LIST_AA+%d\n"
+             "    jmp xor_rotl_into_a\n"
+             "\n"
+             "+   ld TODO_LIST_A+%d\n"
+             "    jnz +\n\n", j, j, j);
+      for (int i = 0; i < 8; i++) {
+        printf("    lit #$%x\n", (expanded_key_table[j * 2] >> (i * 4)) & 0xf);
+        printf("    st OPERAND_B+%d\n", i);
+      }
+      printf("\n"
+             "    lit #1\n"
+             "    st TODO_LIST_A+%d\n"
+             "    jmp add32_into_rc5_a\n"
+             "\n", j);
+      printf("+   ld TODO_LIST_BB+%d\n"
+             "    jnz +\n"
+             "\n"
+             "    ld RC5_A+0\n"
+             "    st ROTL_COUNT+0\n"
+             "    ld RC5_A+1\n"
+             "    st ROTL_COUNT+1"
+             "\n"
+             "    lit #1\n"
+             "    st TODO_LIST_BB+%d\n"
+             "    jmp xor_rotl_into_a\n"
+             "\n"
+             "+   ld TODO_LIST_B+%d\n"
+             "    jnz +\n\n", j, j, j);
+      for (int i = 0; i < 8; i++) {
+        printf("    lit #$%x\n", (expanded_key_table[j * 2 + 1] >> (i * 4)) & 0xf);
+        printf("    st OPERAND_B+%d\n", i);
+      }
+      printf("\n"
+             "    lit #1\n"
+             "    st TODO_LIST_B+%d\n"
+             "    jmp add32_into_rc5_b\n"
+             "\n", j);
+    }
+    printf("+   jmp got_next_key_digit"
+           "\n"
+           "; End of automatically generated code ====================================================\n");
+    return 0;
   }
-}
-#endif
 
 fprintf(stderr, "expanded key table 0: %08x %08x\n", expanded_key_table[0], expanded_key_table[1]);
 fprintf(stderr, "expanded key table 1: %08x %08x\n", expanded_key_table[2], expanded_key_table[3]);
@@ -156,8 +225,15 @@ fprintf(stderr, "expanded key table 2: %08x %08x\n", expanded_key_table[4], expa
       case EOF:
         putchar('\n');
         return 0;
+        
       case '1': case '2': case '3': case '4': case '5': 
       case '6': case '7': case '8': case '9': case '0':
+        // Generate next keystream digit: increment counter, then encrypt nonce+counter
+        // with RC5. Lowest nibble of the result is next keystream digit. If it's
+        // not a valid decimal, discard and repeat the process.
+        //
+        // This approach wastes a lot of key material, but requires less code
+        // which is important for Nibbler.
         int digit;
         for (;;) {
           text[1]++;
@@ -166,13 +242,18 @@ fprintf(stderr, "expanded key table 2: %08x %08x\n", expanded_key_table[4], expa
           if (digit < 10) {
             break;
           }
-        } 
+        }
+        
+        // Encrypt plaintext digit (or decrypt ciphertext digit) by adding their 10's
+        // complements modulo 10. This process is symmetric and reversible.
 //fprintf(stderr, "%d %d %d\n", digit, c - '0', (20 - digit - (c - '0')) % 10);
         digit = (20 - digit - (c - '0')) % 10;
         emit(digit + '0');
         break;
+        
       case ' ': case '\n': case '\r': case '\t':
-        break;
+        break; // Ignore whitespace
+        
       default:
         fprintf(stderr, "Expected plaintext digit, got %c\n", c);
         return -5;
